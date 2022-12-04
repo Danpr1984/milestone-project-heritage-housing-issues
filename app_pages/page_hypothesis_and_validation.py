ca78a126a020a86cbf201c5bc7b053e7f177a52b
Danpr1984 <daniel.porras.rosses@gmail.com>
2022-12-04 23:11:51 +0000

    Streamlit file predict_page adjustments

--- app_pages/page_hypothesis_and_validation.py
+++ app_pages/page_hypothesis_and_validation.py
@@ -5,32 +5,20 @@ def page_hypothesis_and_validation_body():
     st.write("### Project Hypothesis and Validation")
 	
     st.info(
-    f"**1.** We suspect houses with vast `square footing` may have had "
+    f"**1.** We suspect houses with better overall quality will have "
     f"a higher `sales price`: **Correct**. \n "
-    f"- The correlation study on the **House Sale Price Study** supports that; "
-    f"there is a strong correlation between the two. \n\n"
+    f"- `Overal Quality` is the feature with the highest correlation "
+    f"with the target variable `Sale Price`. \n\n"
 
-    f"**2.** We suspect that between houses with similar `square footing`, "
-    f"those with a more recent `Year Built` date may have had a higher "
-    f"`sales price`: **Correct**.\n "
-    f"- The correlation study on the **House Sale Price Study** supports that; "
-    f"there is a moderate correlation between the two. "
-    f"Also noteworthy: houses with a more recent `Year Built` date are "
-    f"usually higher in `Overall Quality` which has stronger correlation "
-    f"to `Sale Price`.\n\n"
+    f"**2.** We suspect houses with larger living area will have a higher sales price."
+    f"**Correct**. \n "
+    f"`Ground Living Area` is the feature with the second highest"
+    f"correlation with the target variable `Sale Price`.\n\n"
 
-    f"**3.** We suspect that between houses with similar `square footing` and "
-    f"`year built` to date, those with a more recent `Remodel` date may "
-    f"have had a higher `sale price`: **Correct**.\n"
-    f"- The correlation study on the **House Sale Price Study** supports that; "
-    f"there is a weak to moderate correlation between the two. "
-    f"Also noteworthy: there is a relationship between houses with a more "
-    f"recent `Remodel` date being higher in `Overall Quality`\n\n"
-
-    f"**4.** We suspect that between houses with similar `square footing`, "
-    f"those with higher `quality and condition` scores may have had a  "
-    f"higher `sales price`: **Correct**.\n "
-    f"- The correlation study on the **House Sale Price Study** supports "
-    f"that; there is a strong correlation between the two variables."
+    f"**3.**We suspect that houses with more recent remodelations"
+    f"`will have a higher sales price.  "
+    f"**Correct**.\n"
+    f"- Eventhough the  `Remodelation Year` has a low correlation with `Sale Price`,"
+    f"there is a strong correlation between `Remodelation Year` and `Overal Quality`"
+    f"which is the feature with strongest correlation with `Sale Price`. n\n"
     )
-
